4e3ca5d9a5ee487acdb51c383925ef5e0d3079d3
becCanDoIt <162036776+becCanDoIt@users.noreply.github.com>
2024-03-06 10:53:27 +1000

    Update Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,3 +1,4 @@
+ # Here is a new update by becCanDoIt
 # Shipping Cost Calculator
 
 ## Input package weight and shipping rate
@@ -9,3 +10,12 @@ shipping_cost = weight * rate
 
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
+
+git config --global user.email "beckayphotography@hotmail.com"
+git config --global user.name "becCanDoIt"
+
+git add .
+git commit -m "added a new line to Shipping_Cost_Calculator.py"
+git status
+
+git push --set-upstream origin shipping_calculator_fixes
